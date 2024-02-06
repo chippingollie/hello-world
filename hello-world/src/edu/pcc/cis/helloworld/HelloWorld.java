9ddd6b7f49efa2c5c8c3fe7f109c16436ca0627b
chippingollie <114436146+chippingollie@users.noreply.github.com>
2024-02-06 12:49:54 -0800

    Add files via upload

---
+++ hello-world/src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package edu.pcc.cis.helloworld;
+
+
+
+public class HelloWorld {
+    public HelloWorld() {
+    }
+
+    public void sayHello() {
+        System.out.println("Hello, World!");
+    }
+}
